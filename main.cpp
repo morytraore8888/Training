66a6dc94575aff1c9d503d86583ed9bc261a6aff
bgrasbgras1982 <bgrasbgras1982@gmail.com>
2026-04-22 03:06:02 +0000

    exercice d'application de bgras

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int main()
 {
-    std::cout << "Bienvenue au depot git" << std::endl; //Effectué par Mory traore
+    /*un nombre dit propair si la somme de ces chiffres pairs est pair 
+    ecrire un programme c++ qui verifie si un nombre saisi par l'utlisateur est 
+    propair ou non 
+    */
     return 0;
 }
