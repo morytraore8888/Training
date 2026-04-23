6bb2fae0ed9af23ea161b538e1ebc0a393a2d295
morytraore8888 <morytraore8888@gmail.com>
2026-04-23 12:44:57 +0200

    resolu par Mory traore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Bienvenue au depot git" << std::endl; //Effectué par Mory traore
+    int nb, som = 0;
+    cout << "donner un nombre" << endl; 
+     cin >> nb;
+     int nombre = nb;
+    if(nombre == 0) cout << 0 << " est propair \n";
+    while(nombre > 0)
+      {
+        int dernier = nombre % 10;
+        if(dernier % 2 == 0) som += dernier;
+        nombre /= 10;
+      }
+    if(som % 2 == 0) cout << "Le nombre " << nb << " est propair \n";   
+    else cout << "Le nombre saisi n'est pas propair"; //Devoir traité par Mory
     return 0;
 }
